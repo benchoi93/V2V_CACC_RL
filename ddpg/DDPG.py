520296a8f2b5a7cb6f27fd479610faae6da3373d
benchoi93 <benchoi93@gmail.com>
2022-05-17 21:06:14 -0400

    init

---
+++ ddpg/DDPG.py
@@ -0,0 +1,134 @@
+from tensorboardX import SummaryWriter
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+from .ReplayBuffer import Replay_buffer
+
+
+class Actor(nn.Module):
+    def __init__(self, state_dim, action_dim, hidden_dim, max_action):
+        super(Actor, self).__init__()
+
+        self.l1 = nn.Linear(state_dim, hidden_dim)
+        self.l2 = nn.Linear(hidden_dim, hidden_dim)
+        self.l3 = nn.Linear(hidden_dim, action_dim)
+
+        self.max_action = max_action
+
+    def forward(self, x):
+        x = F.relu(self.l1(x))
+        x = F.relu(self.l2(x))
+        x = self.max_action * torch.tanh(self.l3(x))
+        return x
+
+
+class Critic(nn.Module):
+    def __init__(self, state_dim, action_dim, hidden_dim):
+        super(Critic, self).__init__()
+
+        self.l1 = nn.Linear(state_dim + action_dim, hidden_dim)
+        self.l2 = nn.Linear(hidden_dim, hidden_dim)
+        self.l3 = nn.Linear(hidden_dim, 1)
+
+    def forward(self, x, u):
+        x = F.relu(self.l1(torch.cat([x, u], -1)))
+        x = F.relu(self.l2(x))
+        x = self.l3(x)
+        return x
+
+
+class DDPG(object):
+    def __init__(self, state_dim, action_dim, hidden_dim, max_action, device, directory, args):
+        self.actor = Actor(state_dim, action_dim, hidden_dim, max_action).to(device)
+        self.actor_target = Actor(state_dim, action_dim, hidden_dim, max_action).to(device)
+        self.actor_target.load_state_dict(self.actor.state_dict())
+        self.actor_optimizer = optim.Adam(self.actor.parameters(), lr=1e-4)
+
+        self.critic = Critic(state_dim, action_dim, hidden_dim).to(device)
+        self.critic_target = Critic(state_dim, action_dim, hidden_dim).to(device)
+        self.critic_target.load_state_dict(self.critic.state_dict())
+        self.critic_optimizer = optim.Adam(self.critic.parameters(), lr=1e-3)
+        self.replay_buffer = Replay_buffer()
+        self.writer = SummaryWriter(directory)
+
+        self.num_critic_update_iteration = 0
+        self.num_actor_update_iteration = 0
+        self.num_training = 0
+
+        self.device = device
+        self.args = args
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        self.directory = directory
+
+        self._cnt = 0
+
+    def select_action(self, state):
+        state = torch.FloatTensor(state.reshape(1, -1)).to(self.device)
+        state = state.view(-1, self.args.num_agents, self.state_dim)
+        return self.actor(state).cpu().data.numpy().flatten()
+
+    def update(self):
+        self._cnt += 1
+
+        for it in range(self.args.update_iteration):
+            # Sample replay buffer
+            b = self.args.batch_size
+            n = self.args.num_agents
+
+            x, y, u, r, d = self.replay_buffer.sample(self.args.batch_size)
+            state = torch.FloatTensor(x).to(self.device).view(b, n, x.shape[-1]//n)
+            action = torch.FloatTensor(u).to(self.device).view(b, n, u.shape[-1]//n)
+            next_state = torch.FloatTensor(y).to(self.device).view(b, n, y.shape[-1]//n)
+            done = torch.FloatTensor(1-d).to(self.device).view(b, n, d.shape[-1]//n)
+            reward = torch.FloatTensor(r).to(self.device).view(b, n, r.shape[-1]//n)
+
+            # Compute the target Q value
+            target_Q = self.critic_target(next_state, self.actor_target(next_state))
+            target_Q = reward + (done * self.args.gamma * target_Q).detach()
+
+            # Get current Q estimate
+            current_Q = self.critic(state, action)
+
+            # Compute critic loss
+            critic_loss = F.mse_loss(current_Q, target_Q)
+            self.writer.add_scalar('Loss/critic_loss', critic_loss, global_step=self.num_critic_update_iteration)
+            # Optimize the critic
+            self.critic_optimizer.zero_grad()
+            critic_loss.backward()
+            self.critic_optimizer.step()
+
+            # Compute actor loss
+            actor_loss = -self.critic(state, self.actor(state)).mean()
+            self.writer.add_scalar('Loss/actor_loss', actor_loss, global_step=self.num_actor_update_iteration)
+
+            # Optimize the actor
+            self.actor_optimizer.zero_grad()
+            actor_loss.backward()
+            self.actor_optimizer.step()
+
+            # Update the frozen target models
+            for param, target_param in zip(self.critic.parameters(), self.critic_target.parameters()):
+                target_param.data.copy_(self.args.tau * param.data + (1 - self.args.tau) * target_param.data)
+
+            for param, target_param in zip(self.actor.parameters(), self.actor_target.parameters()):
+                target_param.data.copy_(self.args.tau * param.data + (1 - self.args.tau) * target_param.data)
+
+            self.num_actor_update_iteration += 1
+            self.num_critic_update_iteration += 1
+
+    def save(self):
+        torch.save(self.actor.state_dict(), self.directory / f'actor_{self._cnt}.pth')
+        torch.save(self.critic.state_dict(), self.directory / 'critic_{self._cnt}.pth')
+        # print("====================================")
+        # print("Model has been saved...")
+        # print("====================================")
+
+    def load(self):
+        self.actor.load_state_dict(torch.load(self.directory + 'actor.pth'))
+        self.critic.load_state_dict(torch.load(self.directory + 'critic.pth'))
+        print("====================================")
+        print("model has been loaded...")
+        print("====================================")
---
+++ main.py
@@ -0,0 +1,169 @@
+import argparse
+import torch
+import numpy as np
+import gym
+import os
+from itertools import count
+from pathlib import Path
+from ddpg.DDPG import DDPG
+from cacc_env.multiCACCenv import multiCACC
+from cacc_env.state_type import state_minmax_lookup
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--mode', default='train', type=str)  # mode = 'train' or 'test'
+    parser.add_argument('--tau',  default=0.005, type=float)  # target smoothing coefficient
+    parser.add_argument('--target_update_interval', default=1, type=int)
+    parser.add_argument('--test_iteration', default=10, type=int)
+
+    parser.add_argument('--learning_rate', default=1e-4, type=float)
+    parser.add_argument('--gamma', default=0.99, type=int)  # discounted factor
+    parser.add_argument('--capacity', default=1000000, type=int)  # replay buffer size
+    parser.add_argument('--batch_size', default=100, type=int)  # mini batch size
+    parser.add_argument('--seed', default=False, type=bool)
+    parser.add_argument('--random_seed', default=9527, type=int)
+    # optional parameters
+
+    parser.add_argument('--sample_frequency', default=2000, type=int)
+    parser.add_argument('--render', default=False, type=bool)  # show UI or not
+    parser.add_argument('--log_interval', default=50, type=int)
+    parser.add_argument('--load', default=False, type=bool)  # load model
+    parser.add_argument('--render_interval', default=100, type=int)  # after render_interval, the env.render() will work
+    parser.add_argument('--exploration_noise', default=0.1, type=float)
+    parser.add_argument('--max_episode', default=100000, type=int)  # num of games
+    parser.add_argument('--print_log', default=5, type=int)
+    parser.add_argument('--update_iteration', default=200, type=int)
+    parser.add_argument('--hidden-dim', default=256, type=int)
+
+    parser.add_argument("--adj-amp", default=1, type=float)
+    parser.add_argument("--speed-reward-coef", default=1, type=float)
+    parser.add_argument("--safe-reward-coef", default=1, type=float)
+    parser.add_argument("--jerk-reward-coef", default=1, type=float)
+    parser.add_argument("--acc-reward-coef", default=1, type=float)
+    parser.add_argument("--energy-reward-coef", default=0.01, type=float)
+
+    parser.add_argument("--shared-reward", default=True, action='store_true')
+    parser.add_argument("--enable-communication", default=False, action='store_true')
+
+    parser.add_argument("--num-agents", default=20, type=int)
+    parser.add_argument("--init_spacing", default=50, type=float)
+    parser.add_argument("--init_speed", default=30, type=float)
+    parser.add_argument("--max-speed", default=120/3.6, type=float)
+    parser.add_argument("--acc-bound", default=5, type=float)
+    parser.add_argument("--keep-duration", default=100, type=int)
+    parser.add_argument("--track-length", default=3000, type=float)
+
+    config = parser.parse_args()
+
+    args = parser.parse_args()
+
+    return args
+
+
+def main(args, device, directory):
+    state_dim = env.observation_space.shape[0] // args.num_agents
+    action_dim = env.action_space.shape[0] // args.num_agents
+    max_action = float(env.action_space.high[0])
+    hidden_dim = args.hidden_dim
+
+    agent = DDPG(state_dim, action_dim, hidden_dim, max_action, device, directory, args)
+    ep_r = 0
+    if args.mode == 'test':
+        agent.load()
+        for i in range(args.test_iteration):
+            state = env.reset()
+            for t in count():
+                action = agent.select_action(state)
+                next_state, reward, done, info = env.step(np.float32(action))
+                ep_r += reward
+                env.render()
+                if done or t >= args.max_length_of_trajectory:
+                    print("Ep_i \t{}, the ep_r is \t{:0.2f}, the step is \t{}".format(i, ep_r, t))
+                    ep_r = 0
+                    break
+                state = next_state
+
+    elif args.mode == 'train':
+        if args.load:
+            agent.load()
+        total_step = 0
+        for i in range(args.max_episode):
+            total_reward = 0
+            step = 0
+            state = env.reset()
+            for t in count():
+                action = agent.select_action(state)
+                action = (action + np.random.normal(0, args.exploration_noise, size=env.action_space.shape[0])).clip(
+                    env.action_space.low, env.action_space.high)
+
+                next_state, reward, done, info = env.step(action)
+                if args.render and i >= args.render_interval:
+                    env.render()
+                agent.replay_buffer.push((state, next_state, action, np.array(reward), np.array(done, bool)))
+
+                state = next_state
+                if all(done):
+                    break
+                step += 1
+                total_reward += np.mean(reward)
+            total_step += step+1
+            print("Total T:{} Episode: \t{} Total Reward: \t{:0.2f} Avg Reward: \t{:0.2f}".format(total_step, i, total_reward, total_reward/step))
+            agent.update()
+           # "Total T: %d Episode Num: %d Episode T: %d Reward: %f
+
+            if i % args.log_interval == 0:
+                agent.save()
+    else:
+        raise NameError("mode wrong!!!")
+
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    script_name = os.path.basename(__file__)
+
+    kwargs = {"num_agents": args.num_agents,
+              "initial_position": np.cumsum(np.ones(args.num_agents)) * args.init_spacing,
+              "initial_speed": np.ones(args.num_agents) * args.init_speed,
+              "dt": 0.1,
+              "acc_bound": (-1 * args.acc_bound, args.acc_bound),
+              "adj_amp": args.adj_amp,
+              "track_length": args.track_length,
+              "max_speed": args.max_speed,
+              "coefs": [args.speed_reward_coef,
+                        args.safe_reward_coef,
+                        args.jerk_reward_coef,
+                        args.acc_reward_coef,
+                        args.energy_reward_coef],
+              "shared_reward": args.shared_reward,
+              "config": args,
+              "state_minmax_lookup": state_minmax_lookup,
+              "enable_communication": args.enable_communication,
+              #   "enable_communication": True,
+              }
+
+    env = multiCACC(**kwargs)
+
+    if args.seed:
+        env.seed(args.random_seed)
+        torch.manual_seed(args.random_seed)
+        np.random.seed(args.random_seed)
+
+    min_Val = torch.tensor(1e-7).float().to(device)  # min value
+
+    model_dir = Path('./models')
+    if not model_dir.exists():
+        curr_run = 'run1'
+    else:
+        exst_run_nums = [int(str(folder.name).split('run')[1]) for folder in
+                         model_dir.iterdir() if
+                         str(folder.name).startswith('run')]
+        if len(exst_run_nums) == 0:
+            curr_run = 'run1'
+        else:
+            curr_run = 'run%i' % (max(exst_run_nums) + 1)
+    directory = model_dir/curr_run / 'logs'
+
+    main(args, device, directory)
